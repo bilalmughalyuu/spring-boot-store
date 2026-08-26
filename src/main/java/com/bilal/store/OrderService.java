077417008de79ed2c782121de397417d09c760bb
Bilal <bilal@atlastechfz.com>
2026-08-26 20:21:01 +0400

    Configuring bean selection with mulitple object creating using primary and service annotation

--- src/main/java/com/bilal/store/OrderService.java
+++ src/main/java/com/bilal/store/OrderService.java
@@ -1,4 +1,6 @@
 package com.bilal.store;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -6,7 +8,12 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
+    public OrderService() {
+
+    }
+
+    @Autowired
+    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
--- src/main/java/com/bilal/store/StripePaymentService.java
+++ src/main/java/com/bilal/store/StripePaymentService.java
@@ -1,5 +1,10 @@
 package com.bilal.store;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service("stripe")
 public class StripePaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
