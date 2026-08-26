da54b68df9b93334d511c8587afb255a5aa9ac82
Bilal <bilal@atlastechfz.com>
2026-08-26 20:37:39 +0400

    Added the application.yaml as replacement for application.properties

--- src/main/java/com/bilal/store/PayPalPaymentService.java
+++ src/main/java/com/bilal/store/PayPalPaymentService.java
@@ -1,11 +1,17 @@
 package com.bilal.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("paypal")
 public class PayPalPaymentService implements PaymentService{
+
+    @Value("${stripe.payPalApiUrl}")
+    private String apiUrl;
+
     @Override
     public void processPayment(double amount) {
         System.out.print("PayPal amount " + amount);
+        System.out.print(apiUrl);
     }
 }
--- src/main/java/com/bilal/store/StripePaymentService.java
+++ src/main/java/com/bilal/store/StripePaymentService.java
@@ -1,13 +1,19 @@
 package com.bilal.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Primary
 @Service("stripe")
 public class StripePaymentService implements PaymentService {
+
+    @Value("${stripe.stripeApiUrl}")
+    private String apiUrl;
+
     @Override
     public void processPayment(double amount) {
         System.out.print("Stripe amount " + amount);
+        System.out.print(apiUrl);
     }
 }
